f781d9d0b9a92cf73fcaa2d6c4777f8ef414fcb4
MVP <102584348+therealhitman37@users.noreply.github.com>
2022-08-17 00:35:57 +0200

    Update 6for.py

--- 6for.py
+++ 6for.py
@@ -11,3 +11,12 @@ for i in range(10):
     print("Iam so sorry")
 
 print("Thank yoou, me too")
+
+#for with else
+
+A = [0,1,2,3,4,5,6,7,8,9]
+
+for a in A:
+    print(a)
+else:
+    print("Out of number")
