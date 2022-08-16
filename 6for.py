d156c62920758454d53f80214a5ef49b6ffa4204
MVP <102584348+therealhitman37@users.noreply.github.com>
2022-08-17 00:14:20 +0200

    Update 6for.py

--- 6for.py
+++ 6for.py
@@ -4,3 +4,10 @@ lovenote = ['father', 'mother', 'sister']
 
 for word in lovenote:
     print('I love ',word)
+
+#for in range
+
+for i in range(10):
+    print("Iam so sorry")
+
+print("Thank yoou, me too")
